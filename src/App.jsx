eaef9fcdfd58e36b1590fe3c9f5be11648213161
MH-liton-360 <liton72524nk@gmail.com>
2025-08-24 23:59:44 +0600

    third

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hospital Management System</h1>
+        <h1 className='text-center' >Hospital Management System</h1>
       </div>
 
     </>
